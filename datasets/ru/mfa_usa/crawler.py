720e81518dcfecc5f0c222af17caa41faa5a0945
Leon Handreke <leonh@ndreke.de>
2024-11-26 17:25:01 +0100

    ru_mfa_usa: Parse published date to Sanction

--- datasets/ru/mfa_usa/crawler.py
+++ datasets/ru/mfa_usa/crawler.py
@@ -12,18 +12,27 @@ REGEX_NAME = re.compile(r"(?P<name_cyrillic>.+)\s\((?P<name_latin>.+)\)")
 # Use to match the "as of" date, which is buried somewhere in a nondescript <span>
 REGEX_PUBLISHED_DATE = re.compile(r"по состоянию на (?P<date>\d+.\d+.\d+)")
 
+
 def crawl(context: Context):
     path = context.fetch_resource("source.html", context.data_url)
     context.export_resource(path, HTML, title=context.SOURCE_TITLE)
     with open(path, "r") as fh:
         doc = html.parse(fh)
 
+    published_date = None
+    published_date_match = REGEX_PUBLISHED_DATE.search(doc.getroot().text_content())
+    if published_date_match:
+        published_date = published_date_match.groupdict()["date"]
+    else:
+        context.log.warning("Could not find published date in page")
+
     table = doc.find(".//table")
     # No table headers, so go through table without helpers
     for row in table.findall(".//tr"):
-        crawl_row(context, row)
+        crawl_row(context, row, published_date)
 
-def crawl_row(context: Context, row: html.Element):
+
+def crawl_row(context: Context, row: html.Element, published_date: str):
     cells = row.findall("./td")
 
     # Skip section header rows (one section per letter of the alphabet)
@@ -54,7 +63,9 @@ def crawl_row(context: Context, row: html.Element):
     # Do not use cell[0] as the key param or the authorityId here because it seems to just be a consecutive number
     # in the alphabetically-sorted table that changes when new entries are inserted.
     sanction = h.make_sanction(context, entity)
-    #h.apply_date(sanction, "listingDate", start_date)
+    # TODO(Leon Handreke): Is this really the right prop, since this is the date of the publication of the whole list,
+    # rather than the date of first inclusion of the person on that list?
+    h.apply_date(sanction, "listingDate", published_date)
 
     context.emit(entity, target=True)
     context.emit(sanction)
